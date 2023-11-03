a304e1b0029db19f1f5002c9563433963425fa73
takedelatoman <kanashiroats@gmail.com>
2023-11-02 23:18:39 -0400

    3ra subida inicio sesión

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,5 +1,4 @@
 <x-guest-layout>
-
     <div class="container position-sticky z-index-sticky top-0">
         <div class="row">
             <div class="col-12">
@@ -7,7 +6,7 @@
             </div>
         </div>
     </div>
-    <main class="main-content  mt-0">
+    <main class="main-content mt-0">
         <section>
             <div class="page-header min-vh-100">
                 <div class="container">
@@ -17,39 +16,38 @@
                                 <div class="oblique-image position-absolute d-flex fixed-top ms-auto h-100 z-index-0 bg-cover me-n8"
                                     style="background-image:url('../assets/img/image-sign-up.jpg')">
                                     <div class="my-auto text-start max-width-350 ms-7">
-                                        <h1 class="mt-3 text-white font-weight-bolder">Start your <br> new journey.</h1>
-                                        <p class="text-white text-lg mt-4 mb-4">Use these awesome forms to login or
-                                            create new account in your project for free.</p>
+                                        <h1 class="mt-3 text-white font-weight-bolder">Inicia tu <br> nuevo camino.</h1>
+                                        <p class="text-white text-lg mt-4 mb-4">Utiliza estos increíbles formularios para iniciar sesión o
+                                            crear una cuenta nueva en tu proyecto de forma gratuita.</p>
                                         <div class="d-flex align-items-center">
                                             <div class="avatar-group d-flex">
                                                 <a href="javascript:;" class="avatar avatar-sm rounded-circle"
                                                     data-bs-toggle="tooltip" data-original-title="Jessica Rowland">
-                                                    <img alt="Image placeholder" src="../assets/img/team-3.jpg"
+                                                    <img alt="Imagen de perfil" src="../assets/img/team-3.jpg"
                                                         class="">
                                                 </a>
                                                 <a href="javascript:;" class="avatar avatar-sm rounded-circle"
                                                     data-bs-toggle="tooltip" data-original-title="Audrey Love">
-                                                    <img alt="Image placeholder" src="../assets/img/team-4.jpg"
+                                                    <img alt="Imagen de perfil" src="../assets/img/team-4.jpg"
                                                         class="rounded-circle">
                                                 </a>
-                                                <a href="javascript:;" class="avatar avatar-sm rounded-circle"
+                                                <a href="javascript:;" class "avatar avatar-sm rounded-circle"
                                                     data-bs-toggle="tooltip" data-original-title="Michael Lewis">
-                                                    <img alt="Image placeholder" src="../assets/img/marie.jpg"
+                                                    <img alt="Imagen de perfil" src="../assets/img/marie.jpg"
                                                         class="rounded-circle">
                                                 </a>
                                                 <a href="javascript:;" class="avatar avatar-sm rounded-circle"
                                                     data-bs-toggle="tooltip" data-original-title="Audrey Love">
-                                                    <img alt="Image placeholder" src="../assets/img/team-1.jpg"
+                                                    <img alt="Imagen de perfil" src="../assets/img/team-1.jpg"
                                                         class="rounded-circle">
                                                 </a>
                                             </div>
-                                            <p class="font-weight-bold text-white text-sm mb-0 ms-2">Join 2.5M+ users
+                                            <p class="font-weight-bold text-white text-sm mb-0 ms-2">Únete a más de 2.5 millones de usuarios
                                             </p>
                                         </div>
                                     </div>
                                     <div class="text-start position-absolute fixed-bottom ms-7">
-                                        <h6 class="text-white text-sm mb-5">Copyright © 2022 Corporate UI Design System
-                                            by Creative Tim.</h6>
+                                        <h6 class="text-white text-sm mb-5">TAKEPHOTOS</h6>
                                     </div>
                                 </div>
                             </div>
@@ -57,34 +55,34 @@
                         <div class="col-md-4 d-flex flex-column mx-auto">
                             <div class="card card-plain mt-8">
                                 <div class="card-header pb-0 text-left bg-transparent">
-                                    <h3 class="font-weight-black text-dark display-6">Sign up</h3>
-                                    <p class="mb-0">Nice to meet you! Please enter your details.</p>
+                                    <h3 class="font-weight-black text-dark display-6">Registrarse</h3>
+                                    <p class="mb-0">¡Gusto en conocerte! Por favor, ingresa tus datos.</p>
                                 </div>
                                 <div class="card-body">
                                     <form role="form" method="POST" action="sign-up">
                                         @csrf
-                                        <label>Name</label>
+                                        <label>Nombre</label>
                                         <div class="mb-3">
                                             <input type="text" id="name" name="name" class="form-control"
-                                                placeholder="Enter your name" value="{{old("name")}}" aria-label="Name"
+                                                placeholder="Ingresa tu nombre" value="{{old("name")}}" aria-label="Nombre"
                                                 aria-describedby="name-addon">
                                             @error('name')
                                                 <span class="text-danger text-sm">{{ $message }}</span>
                                             @enderror
                                         </div>
-                                        <label>Email Address</label>
+                                        <label>Dirección de correo electrónico</label>
                                         <div class="mb-3">
                                             <input type="email" id="email" name="email" class="form-control"
-                                                placeholder="Enter your email address" value="{{old("email")}}" aria-label="Email"
+                                                placeholder="Ingresa tu dirección de correo electrónico" value="{{old("email")}}" aria-label="Correo"
                                                 aria-describedby="email-addon">
                                             @error('email')
                                                 <span class="text-danger text-sm">{{ $message }}</span>
                                             @enderror
                                         </div>
-                                        <label>Password</label>
+                                        <label>Contraseña</label>
                                         <div class="mb-3">
                                             <input type="password" id="password" name="password" class="form-control"
-                                                placeholder="Create a password" aria-label="Password"
+                                                placeholder="Crea una contraseña" aria-label="Contraseña"
                                                 aria-describedby="password-addon">
                                             @error('password')
                                                 <span class="text-danger text-sm">{{ $message }}</span>
@@ -94,30 +92,24 @@
                                             <input class="form-check-input" type="checkbox" name="terms"
                                                 id="terms" required>
                                             <label class="font-weight-normal text-dark mb-0" for="terms">
-                                                I agree the <a href="javascript:;"
-                                                    class="text-dark font-weight-bold">Terms and Conditions</a>.
+                                                Acepto los <a href="javascript:;"
+                                                    class="text-dark font-weight-bold">Términos y Condiciones</a>.
                                             </label>
                                             @error('terms')
                                                 <span class="text-danger text-sm">{{ $message }}</span>
                                             @enderror
                                         </div>
                                         <div class="text-center">
-                                            <button type="submit" class="btn btn-dark w-100 mt-4 mb-3">Sign up</button>
-                                            <button type="button" class="btn btn-white btn-icon w-100 mb-3">
-                                                <span class="btn-inner--icon me-1">
-                                                    <img class="w-5" src="../assets/img/logos/google-logo.svg"
-                                                        alt="google-logo" />
-                                                </span>
-                                                <span class="btn-inner--text">Sign up with Google</span>
-                                            </button>
+                                            <button type="submit" class="btn btn-dark w-100 mt-4 mb-3">Registrarse</button>
+                                            
                                         </div>
                                     </form>
                                 </div>
                                 <div class="card-footer text-center pt-0 px-lg-2 px-1">
                                     <p class="mb-4 text-xs mx-auto">
-                                        Already have an account?
-                                        <a href="{{ route('sign-in') }}" class="text-dark font-weight-bold">Sign
-                                            in</a>
+                                        ¿Ya tienes una cuenta?
+                                        <a href="{{ route('sign-in') }}" class="text-dark font-weight-bold">Inicia
+                                            sesión</a>
                                     </p>
                                 </div>
                             </div>
@@ -127,5 +119,4 @@
             </div>
         </section>
     </main>
-
 </x-guest-layout>
